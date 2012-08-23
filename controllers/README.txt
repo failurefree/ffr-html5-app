0c2a16fcc91d10db26640c0c62b33cd7334f1075
Lance Newby <lance.newby@failurefree.com>
2012-08-23 09:47:46 -0400

    Sample code for media loader
    Test code for preloading media files (audio files in this case).

--- controllers/README.txt
+++ controllers/README.txt
@@ -2,4 +2,4 @@
 |  Controllers		    |
 +===========================+
 
-Controllers for the logic tier of the Failure Free Reading iPad web app. These controllers handle all requests from the users (actors) interaction with the UI elements, and mediate communication between the Models and Views of the app.
+Controllers for the logic tier of the Failure Free Reading iPad web app. These controllers handle all requests from the users (actors) interaction with the UI elements, and mediate communication between the Models and Views of the app. 
--- sample_code/_js/loader.js
+++ sample_code/_js/loader.js
@@ -5,29 +5,7 @@ csoundNames[1] = "dirsEnglish.mp3";
 csoundNames[2] = "dirsSpanish.mp3";
 csoundNames[3] = "dirsQLang.mp3";
 
-/*
-$(video).bind("progress", function(){
-	updateLoadProgress();
-});
-$(video).bind("loadeddata", function(){
-	updateLoadProgress();
-});
-$(video).bind("canplaythrough", function(){
-	updateLoadProgress();
-});
-$(video).bind("playing", function(){
-	updateLoadProgress();
-});
-*/
+var audio = $("#a");
 
-function updateLoadProgress() {
-	if (audio.buffered.length > 0) {
-		var percent = (audio.buffered.end(0) / audio.duration) * 100;
-		$("#load_progress").css({ width: percent + "%" })
-	}
-}
-
-$(document).ready(function(){
-	alert( "OKAY" /*"Start: "+$('#a').buffered.start(0)+" End: "+$('#a').buffered.end(0)*/ );
+alert( "length= "+ audio.buffered.length );
 	//$('#load_progress').html($('#a').buffered.length);
-});
