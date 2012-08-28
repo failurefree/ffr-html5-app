e24ffdf31f4f5f994c1fee3b0592e3b57a6a637a
Lance Newby <lance.newby@failurefree.com>
2012-08-28 17:13:14 -0400

    Sample coding
    Still sample coding for audio, iPad screen orientation.

--- sample_code/_js/loader.js
+++ sample_code/_js/loader.js
@@ -6,7 +6,7 @@ csoundNames[2] = "dirsSpanish.mp3";
 csoundNames[3] = "dirsQLang.mp3";
 */
 
-$(function(){
+$(document).ready(function(){
 
 	/*
 	 * Example how to preload HTML5 video on the iPad (iOS 3.2+)
@@ -38,50 +38,117 @@ $(function(){
 	
 
 	
-	function directions(){ 
+	function createAudioElement(path_and_name){ 
 		$("#media_buffer").append('<audio id="au" controls preload="none">'
-		+'<source src="_media/_audio/dirsEnglish.m4a" type="audio/mp4" />'
-		+'<source src="_media/_audio/dirsEnglish.ogg" type="audio/ogg" />'
-		+'<source src="_media/_audio/dirsEnglish.mp3" type="audio/mpeg" />'
+		+'<source src="'+path_and_name+'.m4a" type="audio/mp4" />'
+		+'<source src="'+path_and_name+'.ogg" type="audio/ogg" />'
+		+'<source src="'+path_and_name+'.mp3" type="audio/mpeg" />'
 		+'<p>Your browser does not support the audio element.</p>'
-		+'</audio>'); 
+		+'</audio>');
+	}
 	
+	function createLanguageButtons(){
+		
+		createAudioElement("_media/_audio/dirsLang-iPad");
+		
 		var audio = document.getElementById("au");
+		
 		// If HTML5 <audio> is supported
 		if(document.createElement("audio").canPlayType){
-			
-			$(audio).bind("progress", function(){
-				updateLoadProgress();
+			// Play Progress ============================//
+			$(audio).bind("timeupdate", function(){
+				var timePercent = (this.currentTime / this.duration) * 100;
+				//$("#play_progress").css({ width: timePercent + "%" })
+				$('#load_progress').html(Math.round(timePercent) +'%');
+				
+				if(timePercent > 49 && timePercent < 68){ $('#eng_btn').addClass('glow'); } else { $('#eng_btn').removeClass('glow'); }
+				if(timePercent > 68 && timePercent < 92){ $('#spn_btn').addClass('glow'); } else { $('#spn_btn').removeClass('glow'); }
+				
 			});
 			
-			$(audio).bind("loadeddata", function(){
-				updateLoadProgress();
-			});
-			
-			$(audio).bind("playing", function(){
-				updateLoadProgress();
-			});
-			
-			$(audio).bind("canplaythrough", function(){
-				updateLoadProgress();
+			// Load Progress ============================//
+			$(audio).bind({
+				progress: function(){
+					updateLoadProgress();
+				},
+				loadeddata: function(){
+					updateLoadProgress();
+				},
+				playing: function(){
+					updateLoadProgress();
+				},
+				canplaythrough: function(){
+					updateLoadProgress();
+				}
 			});
 			
 			function updateLoadProgress(){
 				if(audio.buffered.length > 0){
 						var percent = Math.round((audio.buffered.end(0) / audio.duration) * 100);
-						$('#load_progress').html(percent +'%');
+						
+						//$('#load_progress').html(percent +'%');
 				}
 			}
-		}
-		$("#start_but").click(function(){
+			 
+		} else { /* If HTML 5 <audio> is not support */ }
+	}
+
+	// Click handler for web app START button
+	$("#start_btn").bind({
+		click: function(){
+
+			/* 
+			 * Create instructional language selection buttons
+			 * add them to the DOM (current page), and add
+			 * event listeners.
+			 */
+			$('#main_content').append('<div class="box"><p>Please select your language:</p></div>'
+				+'<div id="lang_wrapper" class="box" disable="disabled">'
+				+'<div id="eng_btn" class="lang_button">English</div>'
+				+'<div id="spn_btn" class="lang_button">Espa&ntilde;ol</div>'
+				+'</div>');
+			
+			createLanguageButtons();
+			
+			// Remove me from the DOM (remove start button from screen).
+			$(this).remove();
+			
+			// Add application tools to DOM (in tool_bar container at bottom of UI).
+			$('#tool_bar').append('<div id="help_btn" class="btn_S">?</div><div id="close_btn" class="btn_S">X</div>');
+			
+			/*
+			 * Play audio instructions for selecting a language.
+			 */
+			var audio = document.getElementById("au");
+			// If HTML5 <audio> is supported
+			if(document.createElement("audio").canPlayType){
 				audio.load();
 				audio.play();
-				$('#lang_wrapper').removeClass('hidden').addClass('box');
-				// make audio player visible
 				
-				$(this).addClass('hidden');
-		});
-	}
+				// Handlers for language selection buttons
+				$("#eng_btn").bind({ // English button
+					click: function() {
+						$(this).addClass("glow");
+					}
+				});
+				
+				$("#spn_btn").bind({ // Spanish button
+					click: function() {
+						$(this).addClass("glow");
+					}
+				});
+				
+				$("#help_btn").bind({ // Spanish button
+					click: function() {
+						audio.play();
+					}
+				});
+			}
+			
+			$('#lang_wrapper').removeAttr('disabled');
+			$('#eng_btn').removeClass("glow");
+			$('#spn_btn').removeClass("glow");
+		}
+	});
 	
-	directions();
 });
